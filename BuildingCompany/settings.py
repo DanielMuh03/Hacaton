bccd412f517c215e783badcd519aaf6e3bcee80d
Daniel <danielmuhamedzanov291@gmil.com>
2022-01-31 16:08:42 +0600

    probuem git hub

--- BuildingCompany/settings.py
+++ BuildingCompany/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'BuildingCompany.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bookshop_db',
+        'USER': 'hello',
+        'PASSWORD': '1',
+        'HOST': 'localhost',
+        'PORT': 5432
     }
 }
 
