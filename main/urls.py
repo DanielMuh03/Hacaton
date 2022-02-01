a586cf8e34166dfc6f611d310c320ae80a3fc60d
Khasan312 <hasan.aliev.2000@mail.ru>
2022-02-01 12:08:14 +0600

    few changes

--- main/urls.py
+++ main/urls.py
@@ -6,7 +6,5 @@ urlpatterns = [
     path('', index, name='home'),
     path('gallery', gallery, name='gallery'),
     path('full-width', full_width, name='full'),
-    path('sidebar', side_bar, name='side-bar'),
-    path('side-right', sidebar_right, name='sidebar-right'),
     path('basic-grid', basic_grid, name='basic-grid'),
 ]
--- main/views.py
+++ main/views.py
@@ -13,14 +13,6 @@ def full_width(request):
     return render(request, 'full-width.html')
 
 
-def side_bar(request):
-    return render(request, 'sidebar-left.html')
-
-
-def sidebar_right(request):
-    return render(request, 'sidebar-right.html')
-
-
 def basic_grid(request):
     return render(request, 'basic-grid.html')
 
