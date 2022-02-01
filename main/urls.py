d52f03550b7b1e63d43c92d826d75548df94aae7
Khasan312 <hasan.aliev.2000@mail.ru>
2022-02-01 10:53:54 +0600

    all templates

--- main/urls.py
+++ main/urls.py
@@ -5,4 +5,8 @@ from .views import *
 urlpatterns = [
     path('', index, name='home'),
     path('gallery', gallery, name='gallery'),
+    path('full-width', full_width, name='full'),
+    path('sidebar', side_bar, name='side-bar'),
+    path('side-right', sidebar_right, name='sidebar-right'),
+    path('basic-grid', basic_grid, name='basic-grid'),
 ]
--- main/views.py
+++ main/views.py
@@ -9,3 +9,18 @@ def gallery(request):
     return render(request, 'gallery.html')
 
 
+def full_width(request):
+    return render(request, 'full-width.html')
+
+
+def side_bar(request):
+    return render(request, 'sidebar-left.html')
+
+
+def sidebar_right(request):
+    return render(request, 'sidebar-right.html')
+
+
+def basic_grid(request):
+    return render(request, 'basic-grid.html')
+
