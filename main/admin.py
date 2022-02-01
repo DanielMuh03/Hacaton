193ff59c1b08edaccbedccf9d0621612d7ecf614
Daniel <danielmuhamedzanov291@gmil.com>
2022-02-01 12:35:33 +0600

    update code

--- main/admin.py
+++ main/admin.py
@@ -3,6 +3,13 @@ from django.contrib import admin
 from .models import *
 
 
+class ImageInlineAdmin(admin.TabularInline):
+    model = Image
+    fields = ('image', )
+    max_num = 5
+
+@admin.register(Build)
+class BuildsAdmin(admin.ModelAdmin):
+    inlines = [ImageInlineAdmin,]
+
 admin.site.register(Category)
-admin.site.register(Build)
-admin.site.register(Image)
